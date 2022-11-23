ad0ce17bced029e933c43d7fc882089b22655849
apton-anandhu-or <anandhu.or@aptonmworks.com>
2022-11-23 17:57:12 +0530

    Minor Changes

--- src/Components/NameSection.js
+++ src/Components/NameSection.js
@@ -9,11 +9,11 @@ export default function NameSection() {
       <p className='position'>Frontend Developer</p>
       <p className='website'>laurasmith.website</p>
       <div className='Buttons'>
-        <button className='Btn-1'>
+        <button className='Btn-1' href='#'>
           <img src={Email} alt='Email Icon' width='15px' />
           Email
         </button>
-        <button className='Btn-2'>
+        <button className='Btn-2' href='#'>
         <img src={Linkedin} alt='Email Icon' width='15px' />
           Linkedin
         </button>
